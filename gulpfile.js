68e0e43bb43fb68cbd5341e6d634cdc4f7d36930
Dara Dolan <dara.dolan@modeva.com>
2014-07-04 09:39:56 +0100

    A change to test ssh

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp'),
     gutil = require('gulp-util');
 
 gulp.task('log', function() {
-   gutil.log('Workflows are awesome'); 
+   gutil.log('Workflows are nice to use'); 
 });
